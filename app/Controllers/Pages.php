66982ab95e6fbae7ab1696d1d2d464a704cb0956
hanasoke@gmail.com <hanasoke@gmail.com>
2025-01-23 20:56:26 +0700

    View Layouts

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -10,9 +10,7 @@ class Pages extends BaseController
             'title' => 'Home | Coding Wit Hanas',
             'test' => ['one', 'two','three']
         ];
-        echo view('layout/header', $data);
-        echo view('pages/home', );
-        echo view('layout/footer');
+        return view('pages/home', $data);
     }
 
     public function about()
@@ -20,8 +18,26 @@ class Pages extends BaseController
         $data = [
             'title' => 'About Me'
         ];
-        echo view('layout/header', $data);
-        echo view('pages/about');
-        echo view('layout/footer');
+        return view('pages/about', $data);
+    }
+
+    public function contact() 
+    {
+        $data = [
+            'title' => 'Contact Us',
+            'alamat' => [
+                [
+                    'tipe' => 'rumah',
+                    'alamat' => 'Jl. abc No. 123',
+                    'kota' => 'Bandung'
+                ], 
+                [
+                    'tipe' => 'Kantor',
+                    'alamat' => 'Jl. Setiabudi No. 193',
+                    'kota' => 'Bandung'
+                ]
+            ]
+        ];
+        return view('pages/contact', $data);
     }
 }
--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -1,11 +1,12 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
 <div class="container">
     <div class="row">
         <div class="col">
             <h1>Hanas Bayu Pratama, B.ICT</h1>
-            <?php 
-                d($test)
-            ?>
         </div>
     </div>
 </div>
+<?= $this->endSection(); ?>
 
